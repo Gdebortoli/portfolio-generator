1e5e67d5d17e3a8c074680614cc44e5ecedc0265
“Gigi <debortolg@gmail.com>
2022-10-04 23:49:34 -0700

    added when property to about me prompt

--- app.js
+++ app.js
@@ -23,29 +23,43 @@ const promptUser = () => {
             message: 'What is your name? *Required*',
             validate: nameInput => {
                 if (nameInput) {
-                  return true;
+                    return true;
                 } else {
-                  console.log('Please enter your name!');
-                  return false;
+                    console.log('Please enter your name!');
+                    return false;
                 }
-              }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username *Required*', 
+            message: 'Enter your GitHub Username *Required*',
             validate: githubInput => {
                 if (githubInput) {
-                  return true;
+                    return true;
                 } else {
-                  console.log('Please enter your Github Username!');
-                  return false;
+                    console.log('Please enter your Github Username!');
+                    return false;
                 }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
@@ -67,26 +81,28 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'projectname',
-            message: 'What is the name of your project?', 
+            message: 'What is the name of your project? *Required*',
             validate: projectnameInput => {
                 if (projectnameInput) {
-                  return true;
+                    return true;
                 } else {
-                  console.log('Please enter your name!');
-                  return false;
+                    console.log('Please enter your name!');
+                    return false;
                 }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project *Required*', 
+            message: 'Provide a description of the project *Required*',
             validate: descriptionInput => {
                 if (descriptionInput) {
-                  return true;
+                    return true;
                 } else {
-                  console.log('Please provide a description of your project!');
-                  return false;
+                    console.log('Please provide a description of your project!');
+                    return false;
                 }
+            }
         },
         {
             type: 'checkbox',
@@ -97,14 +113,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the Github link to your project. *Required*', 
+            message: 'Enter the Github link to your project. *Required*',
             validate: linkInput => {
                 if (linkInput) {
-                  return true;
+                    return true;
                 } else {
-                  console.log('Please enter your Github Link!');
-                  return false;
+                    console.log('Please enter your Github Link!');
+                    return false;
                 }
+            }
         },
         {
             type: 'confirm',
@@ -126,7 +143,7 @@ const promptProject = portfolioData => {
             if (projectData.confirmAddProject) {
                 return promptProject(portfolioData);
             } else {
-            // if they choose not to add more portfolio data it will return false and stop retrieving data   
+                // if they choose not to add more portfolio data it will return false and stop retrieving data   
                 return portfolioData;
             }
         });
@@ -138,4 +155,4 @@ promptUser()
     .then(promptProject)
     .then(portfolioData => {
         console.log(portfolioData);
-      });
+    });
